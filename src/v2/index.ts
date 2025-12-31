b1bfa6ed35923e6f78bf9e3ad0d5305be1766ea7
Stanislav Sysoev <d4rkr00t@gmail.com>
2025-12-31 16:30:44 +1100

    wip2

--- src/v2/index.ts
+++ src/v2/index.ts
@@ -5,6 +5,78 @@ import fs from "node:fs";
 // const beforeTracePath = "./example-traces/github-trace-1.json";
 const afterTracePath = "./example-traces/github-trace-2.json";
 
+// const functionCall = {
+//   args: {
+//     data: {
+//       columnNumber: 4097,
+//       frame: "638DDBE511DFD7E5BDA4FC78E9BF1B23",
+//       functionName: "c",
+//       isolate: "13708300334584978572",
+//       lineNumber: 1,
+//       sampleTraceId: 2044053812858358,
+//       scriptId: "591",
+//       url: "https://github.githubassets.com/assets/43784-4652ae97a661.js",
+//     },
+//   },
+//   cat: "devtools.timeline",
+//   dur: 135,
+//   name: "FunctionCall",
+//   ph: "X",
+//   pid: 5447,
+//   tdur: 131,
+//   tid: 8661476,
+//   ts: 542597631887,
+//   tts: 8452943,
+// };
+
+// const functionCall = {
+//   args: {
+//     data: {
+//       columnNumber: 7438,
+//       frame: "638DDBE511DFD7E5BDA4FC78E9BF1B23",
+//       functionName: "N",
+//       isolate: "13708300334584978572",
+//       lineNumber: 1,
+//       sampleTraceId: 2044053812855387,
+//       scriptId: "591",
+//       url: "https://github.githubassets.com/assets/43784-4652ae97a661.js",
+//     },
+//   },
+//   cat: "devtools.timeline",
+//   dur: 316,
+//   name: "FunctionCall",
+//   ph: "X",
+//   pid: 5447,
+//   tdur: 316,
+//   tid: 8661476,
+//   ts: 542597780115,
+//   tts: 8596863,
+// };
+
+// const functionCall = {
+//   args: {
+//     data: {
+//       columnNumber: 1948,
+//       frame: "638DDBE511DFD7E5BDA4FC78E9BF1B23",
+//       functionName: "include_fragment_element_IncludeFragmentElement",
+//       isolate: "13708300334584978572",
+//       lineNumber: 259,
+//       sampleTraceId: 2044053812857943,
+//       scriptId: "597",
+//       url: "https://github.githubassets.com/assets/52430-2f44a4a36933.js",
+//     },
+//   },
+//   cat: "devtools.timeline",
+//   dur: 13,
+//   name: "FunctionCall",
+//   ph: "X",
+//   pid: 5447,
+//   tdur: 12,
+//   tid: 8661476,
+//   ts: 542597694992,
+//   tts: 8515437,
+// };
+//
 const functionCall = {
   args: {
     data: {
@@ -13,20 +85,20 @@ const functionCall = {
       functionName: "c",
       isolate: "13708300334584978572",
       lineNumber: 1,
-      sampleTraceId: 2044053812858358,
+      sampleTraceId: 2044053812858035,
       scriptId: "591",
       url: "https://github.githubassets.com/assets/43784-4652ae97a661.js",
     },
   },
   cat: "devtools.timeline",
-  dur: 135,
+  dur: 1743,
   name: "FunctionCall",
   ph: "X",
   pid: 5447,
-  tdur: 131,
+  tdur: 1743,
   tid: 8661476,
-  ts: 542597631887,
-  tts: 8452943,
+  ts: 542597701221,
+  tts: 8521445,
 };
 
 function main() {
@@ -124,21 +196,45 @@ function buildTraceForFunctionCall(profileData, functionCallEvent) {
   console.log(samplesIndexes);
   console.log(samplesIndexes.map((idx) => profile.samples[idx]));
 
-  const stack = samplesIndexes.map((idx) => {
-    const nodeIdx = profile.samples[idx];
-    return getStack(profile.nodes, profile.nodes[nodeIdx - 1]);
-  });
+  const stack = samplesIndexes
+    .map((idx) => {
+      const nodeIdx = profile.samples[idx];
+      const [stack, seenFunction] = getStack(
+        profile.nodes,
+        profile.nodes[nodeIdx - 1],
+        functionCall,
+      );
+
+      if (!seenFunction) {
+        return null;
+      }
+
+      return [
+        profile.timeDeltas[idx] / 1000, // -> ms
+        stack,
+      ];
+    })
+    .filter(Boolean);
   console.log("Stack:");
   console.log(JSON.stringify(stack, null, 2));
 }
 
-function getStack(profileNodes, node) {
+function getStack(profileNodes, node, functionCall) {
+  const maybeSeenFunction =
+    +node.callFrame.scriptId === +functionCall.args.data.scriptId &&
+    node.callFrame.functionName === functionCall.args.data.functionName;
+
   if (!node.parent) {
-    return [node];
+    return [[node], maybeSeenFunction];
   }
 
-  const parents = getStack(profileNodes, profileNodes[node.parent - 1]);
-  parents.push(node);
+  const [stack, seenFunction] = getStack(
+    profileNodes,
+    profileNodes[node.parent - 1],
+    functionCall,
+  );
 
-  return parents;
+  stack.push(node);
+
+  return [stack, seenFunction || maybeSeenFunction];
 }
